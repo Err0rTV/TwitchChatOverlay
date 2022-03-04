7f88ab232bf6ca73c119da6c16fee1130aa2d62e
Syntax_Err0r <Syntax_Err0r@Err0rTV>
2022-03-04 11:08:06 +0100

    remove message deleted by moderators

--- OBSTwitchChat.js
+++ OBSTwitchChat.js
@@ -43,6 +43,15 @@ function start_chat(login) {
 		});
 		client.connect().catch(console.error);
 
+
+		client.on('messagedeleted', (channel, _username2, deletedMessage, tags) => {
+			console.log(channel);
+			console.log(_username2);
+			console.log(deletedMessage);
+			console.log(tags);
+			delMsg(tags["target-msg-id"]);
+		});
+/*		{
 		client.on('message', (channel, tags, message, self) => {
 			if (tags.emotes != null) {
 				let map = new Array();
@@ -268,6 +277,14 @@ async function showMsg(id, badges, name, nameColor, msg) {
 	add(id, txt + "<br>");
 }
 
+function delMsg(id) {
+	let message = document.getElementById(id);
+	if (message != null) {
+		if (message.parentNode) {
+			message.parentNode.removeChild(message);
+		}
+	}
+}
 
 async function testmsg() {
 	const dumyText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.";
