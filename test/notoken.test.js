a201a4de892dd32bd62004044aa734329c5c9dc7
Syntax_Err0r <Syntax_Err0r@Err0rTV>
2022-08-12 16:51:37 +0200

    initial test

---
+++ test/notoken.test.js
@@ -0,0 +1,33 @@
+cpuppeteer = require('./cpuppeteer.js')
+
+// let exp = ['please provide a valid token']
+describe.each([
+	['--chatbox-testMode: 1;', "",['please provide a valid token']],
+	['--chatbox-testMode: 2;', "",['please provide a valid token']],
+]) ('%s, %s', (testMode, token, expected) => {
+	var puppet;
+	beforeAll(async () => {
+		puppet = new cpuppeteer();
+		await puppet.init()
+		await puppet.goto('../src/OBSTwitchChat.html')
+		await puppet.page.addStyleTag({
+			content: `
+				.chat {
+				  ${testMode} ${token}
+			  }`})
+		await new Promise((r) => setTimeout(r, 5000));
+	}, 90 * 1000)
+
+	test(`le message dans la console doit être "${expected}"`, async () => {
+		await expect(puppet.output).toEqual((expected))
+	});
+
+	test('no errors', async () => {
+		await expect(puppet.errors).toHaveLength(0);
+	});
+
+	afterAll(async () => {
+		await puppet.close();
+	});
+
+});
